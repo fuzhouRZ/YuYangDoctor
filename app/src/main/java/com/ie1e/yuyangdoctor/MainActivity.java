8ca93b43c86e3375c7ad1c4ebabcca102f54a8df
Zhengxz <feng2976522@163.com>
2016-07-06 11:34:00 +0800

    0.1 新建项目

---
+++ app/src/main/java/com/ie1e/yuyangdoctor/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ie1e.yuyangdoctor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by zhengxizhen on 16/7/6.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
